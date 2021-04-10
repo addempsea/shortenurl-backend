599db7561dd5de35249ff3659fd8e8c43de4fb0d
addempsea <babatundeademola4@gmail.com>
2021-04-10 11:59:27 +0100

    chore(introspection): allow introspection

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { resolvers, typeDefs } from './app/graphql';
 import Logger from './config/logger';
-import config, { appConfig } from './config';
+import { appConfig } from './config';
 
 global.logger = Logger.createLogger({ label: 'BACKDROP_BACKEND' });
 const app = express();
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-  introspection: config.NODE_ENV !== 'production'
+  resolvers
 });
 
 appConfig(app, server);
