33a3a3dc9d14c0ba1194f30a6ad0a9ef1a507c54
addempsea <babatundeademola4@gmail.com>
2021-04-10 11:55:59 +0100

    fix(port): rename port env

--- config/env/index.js
+++ config/env/index.js
@@ -2,7 +2,7 @@
 import rootPath from 'app-root-path';
 import 'dotenv/config';
 
-const { BACKDROP_PORT: PORT, BACKDROP_NODE_ENV: NODE_ENV } = process.env;
+const { PORT, BACKDROP_NODE_ENV: NODE_ENV } = process.env;
 
 export default {
   ...process.env,
